8e3e7bd77b9aee147f54600b5b0e65929232102e
MARTINS2309 <martins2309@gmail.com>
2022-03-23 12:42:11 +0000

    refactored app to reduce API calls

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,82 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
 import './App.css';
 import {StockEventsTable} from './components/StockEventsTable';
 import {Navibar} from './components/Navbar';
 import {Home} from './components/Home';
 
-
-//fetch all stock events
-//separate by different products
-//Display them
 export const App = () => {
-  const [fetchedProducts, setFectechedProducts] = useState(
-    [
-      {
-        id: 1,
-        attributes:{
-          name: "App failed to connect",
-          createdAt:	"2022-02-18T10:47:49.151Z",
-          updatedAt:	"2022-02-18T10:58:40.866Z",
-          publishedAt:	"2022-02-18T10:58:40.865Z", 
-        }
-      }
-    ] 
-  );
-  const [fetchedStockEvents, setFetchedStockEvents] = useState(
-    [
-      {
-        id: 1,
-        attributes:{
-          type: 'add',
-          qty: 100,
-          createdAt:	"2022-02-18T10:59:02.976Z",
-          updatedAt:	"2022-02-18T10:59:23.930Z",
-          publishedAt:	"2022-02-18T10:59:23.929Z",
-          product: {
-            data: {
-              id: 1,
-              attributes:{
-                name: "App failed to connect",
-                createdAt:	"2022-02-18T10:47:49.151Z",
-                updatedAt:	"2022-02-18T10:58:40.866Z",
-                publishedAt:	"2022-02-18T10:58:40.865Z", 
-              }
-            }
-          }
-        }
-      }
-    ]
-  );
-  const [isSubscribed, setIsSubscribed] = useState(true);
-
-  useEffect(()=>{
-    const fetchProducts = async () => {
-      const productsRes =  await axios({
-        method: 'GET',
-        url: 'http://localhost:1337/api/products'
-      })
-      setFectechedProducts(productsRes.data.data)
-    }
-
-    const fetchStockEvents = async () => {
-      const stockEventsRes = await axios({
-        method: 'GET',
-        url: 'http://localhost:1337/api/stockevents',
-        params: {
-          populate: '*'
-        },
-      })
-      setFetchedStockEvents(stockEventsRes.data.data)
-    }
-
-    if (isSubscribed){
-      fetchProducts().catch(console.error)
-      fetchStockEvents().catch(console.error)
-      setIsSubscribed(false)
-    }
-  },[]);
 
   return (
     <div className="App" data-testid="app">
@@ -84,10 +13,9 @@ export const App = () => {
           <Navibar />
           <Routes>
             <Route path='/' element={<Home/>} />
-            <Route path="/stockevents" element={<StockEventsTable products={fetchedProducts} stockEvents={fetchedStockEvents} />} />
+            <Route path="/stockevents" element={< StockEventsTable />} />
           </Routes>
         </BrowserRouter>
     </div>
   )
-
 }
